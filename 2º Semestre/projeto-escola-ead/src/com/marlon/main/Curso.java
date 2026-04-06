d92b0c5f65db54645db9b522df175dacb7593319
Marlon <marlon.da.silva996@gmail.com>
2026-04-06 15:18:22 -0300

    feat(Curso): estruturando a classe Aluno e progredindo com a resolução do desafio proposto na aula

---
+++ 2º Semestre/projeto-escola-ead/src/com/marlon/main/Curso.java
@@ -0,0 +1,32 @@
+package com.marlon.main;
+
+public class Curso {
+    private int codigo;
+    private String nome;
+    private int duracao;
+
+    public Curso(int codigo, String nome, int duracao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.duracao = duracao;
+    }
+
+    public int getCodigo() { return codigo; }
+
+    public String getNome() { return nome; }
+
+    public int getDuracao() { return duracao; }
+
+    public void setCodigo(int codigo) { this.codigo = codigo; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public void setDuracao(int duracao) { this.duracao = duracao; }
+
+    public void exibeDados() {
+        System.out.println("--- INFORMAÇÕES DO CURSO ---");
+        System.out.printf("Código: : %d%n", codigo);
+        System.out.printf("Nome: %s%n", nome);
+        System.out.printf("Duração: %d horas.", duracao);
+    }
+}
