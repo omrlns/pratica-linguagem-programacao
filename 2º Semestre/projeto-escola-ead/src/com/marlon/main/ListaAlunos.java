1de7cab87f9855e316009420dc4059f671353a1d
Marlon <marlon.da.silva996@gmail.com>
2026-04-06 22:38:00 -0300

    feat(ListaAlunos): criando a classe ListaAlunos para exibir os alunos matriculados

---
+++ 2º Semestre/projeto-escola-ead/src/com/marlon/main/ListaAlunos.java
@@ -0,0 +1,37 @@
+package com.marlon.main;
+
+public class ListaAlunos {
+    private Aluno[] alunos;
+    private int totalAlunos;
+
+    public ListaAlunos(int capacidade) {
+        // vetor informando a quantidade de cadeiras vazias
+        this.alunos = new Aluno[capacidade];
+        this.totalAlunos = 0;
+    }
+
+    public boolean adicionarAlunos(Aluno alunoNovo) {
+        // percorrendo a lista apenas até onde tem alunos gravados
+        for (int i = 0; i < this.totalAlunos; i++) {
+            // verificando a duplicidade
+            if (this.alunos[i].getCodigo() == alunoNovo.getCodigo()) {
+                System.out.println("[ERRO] Já existe um aluno com este código!");
+                return false; // interrompe o método e não deixa cadastrar o aluno
+            }
+        }
+        // caso o laço termine e não ache ninguém igual, a matrícula poderá ser realizada
+        this.alunos[this.totalAlunos] = alunoNovo; // guarda o aluno novo na primeira vaga livre
+        this.totalAlunos++; // aumenta a contagem de alunos matriculados
+        return true;
+    }
+
+    public void exibirLista() {
+        System.out.println("----- LISTA DE ESTUDANTES MATRICULADOS -----");
+        // o laço vai percorrer apenas os alunos que existem na lista
+        for (int i = 0; i < this.totalAlunos; i++) {
+            System.out.printf("\nAluno #%d%n", i + 1);
+            // o aluno da posição i é selecionado e assim ele exibe os seus dados
+            this.alunos[i].exibeDados();
+        }
+    }
+}
