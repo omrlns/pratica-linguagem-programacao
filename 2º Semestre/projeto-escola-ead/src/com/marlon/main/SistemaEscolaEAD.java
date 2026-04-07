61a1b9d33357b453de690fa969db93cfa9bf9e0d
Marlon <marlon.da.silva996@gmail.com>
2026-04-07 17:22:40 -0300

    feat(SistemaEscolaEAD): ajustando os cases do menu e aplicando a lógica para criar aluno e salvar aluno

--- 2º Semestre/projeto-escola-ead/src/com/marlon/main/SistemaEscolaEAD.java
+++ 2º Semestre/projeto-escola-ead/src/com/marlon/main/SistemaEscolaEAD.java
@@ -23,46 +23,66 @@ public class SistemaEscolaEAD {
 
             switch (opcaoMenu) {
                 case 1:
-                    // exibir a lista de alunos
+                    listaPrincipal.exibirLista();
                     break;
                 case 2:
-                    Aluno alunoTeste = new Aluno(1, "Marlon da Silva", "29/05/2003", "marlon@gmail.com", "marlon@123");
+                    // Aluno alunoTeste = new Aluno(1, "Marlon da Silva", "29/05/2003", "marlon@gmail.com", "marlon@123");
+                    System.out.println("----- CADASTRO DE ALUNO(A) -----\n");
 
-                    System.out.println("Quantos cursos você quer cadastrar? ");
-                    int quantidadeCursos = sc.nextInt();
+                    System.out.println("Nome Completo: ");
+                    String nome = sc.nextLine();
+                    System.out.println("Código: ");
+                    int codigo = sc.nextInt();
+                    sc.nextLine(); // limpeza de bufferr
+                    System.out.println("Data de Nascimento: ");
+                    String dataNascimento = sc.next();
+                    System.out.println("Email: ");
+                    String email = sc.next();
+                    System.out.println("Senha: ");
+                    String senha = sc.next();
 
-                    sc.nextLine(); // limpa o "enter" após ler a quantidade de cursos
+                    Aluno novoAluno = new Aluno(codigo, nome, dataNascimento, email, senha);
 
-                    for (int i = 0; i < quantidadeCursos; i++) {
-                        System.out.printf("--- CADASTRO DO CURSO #%d ---%n", i + 1);
+                    boolean cadastroComSucesso = listaPrincipal.adicionarAlunos(novoAluno);
 
-                        System.out.println("Nome: ");
-                        String nomeCurso = sc.nextLine();
-                        System.out.println("Código: ");
-                        int codigo = sc.nextInt();
-                        System.out.println("Duração/Horas: ");
-                        int duracao = sc.nextInt();
+                    if (cadastroComSucesso == true) {
+                        System.out.println("Quantos cursos você quer cadastrar? ");
+                        int quantidadeCursos = sc.nextInt();
 
-                        sc.nextLine(); // limpa o "enter" após ler a duração
+                        sc.nextLine(); // limpa o "enter" após ler a quantidade de cursos
 
-                        Curso novoCurso = new Curso(codigo, nomeCurso, duracao);
+                        for (int i = 0; i < quantidadeCursos; i++) {
+                            System.out.printf("--- CADASTRO DO CURSO #%d ---%n", i + 1);
 
-                        boolean matriculou = false;
+                            System.out.println("Nome: ");
+                            String nomeCurso = sc.nextLine();
+                            System.out.println("Código: ");
+                            int codigoCurso = sc.nextInt();
+                            System.out.println("Duração/Horas: ");
+                            int duracao = sc.nextInt();
 
-                        while (!matriculou) {
-                            System.out.println("Em qual semestre você deseja matricular? (1-5): ");
-                            int semestre = sc.nextInt() - 1;
+                            sc.nextLine(); // limpa o "enter" após ler a duração
 
-                            System.out.println("Qual a posição da matéria no semestre? (1-5): ");
-                            int materia = sc.nextInt() - 1;
+                            Curso novoCurso = new Curso(codigoCurso, nomeCurso, duracao);
 
-                            sc.nextLine(); // limpa o "enter" após ler as coordenadas
+                            boolean matriculou = false;
 
-                            // guardando as informações do curso dentro do objeto alunoTeste
-                            matriculou = alunoTeste.adicionarCurso(novoCurso, semestre, materia);
+                            while (!matriculou) {
+                                System.out.println("Em qual semestre você deseja matricular? (1-5): ");
+                                int semestre = sc.nextInt() - 1;
+
+                                System.out.println("Qual a posição da matéria no semestre? (1-5): ");
+                                int materia = sc.nextInt() - 1;
+
+                                sc.nextLine(); // limpa o "enter" após ler as coordenadas
+
+                                // guardando as informações do curso dentro do objeto alunoTeste
+                                matriculou = novoAluno.adicionarCurso(novoCurso, semestre, materia);
+                            }
                         }
+                        System.out.println("[SUCESSO] O cadastro de aluno e cursos foram finalizado com sucesso!");
+                        novoAluno.exibeDados();
                     }
-                    alunoTeste.exibeDados();
                     break;
                 case 3:
                     System.out.println("Saindo do sistema...");
