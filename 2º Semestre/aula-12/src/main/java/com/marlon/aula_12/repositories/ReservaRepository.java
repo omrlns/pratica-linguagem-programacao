924bd435c635352c9220a05c17ab03ed23c17e92
Marlon <marlon.da.silva996@gmail.com>
2026-05-13 18:30:07 -0300

    feat(java): construindo os /repositorys da atividade ead

--- 2º Semestre/aula-12/src/main/java/com/marlon/aula_12/repositories/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marlon.aula_12.repositories;
-
-import com.marlon.aula_12.models.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
--- 2º Semestre/aula-12/src/main/java/com/marlon/aula_12/repositories/ServicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marlon.aula_12.repositories;
-
-import com.marlon.aula_12.models.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServicoRepository extends JpaRepository<Servico, Long> {
-}
