d69b3e68786265f8488f9a792e178e212b106ba5
Marlon <marlon.da.silva996@gmail.com>
2026-09-08 22:01:51 -0300

    feat: configura roteamento de páginas com react-router-dom no app

---
+++ Programação III/Aula 04/Desafios/desafio-aula-zeroquatro/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Lista from "./pages/Lista";
+import NotFound from "./pages/NotFound";
+import "./App.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <Header title="Desafio da Aula 04" />
+
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/lista" element={<Lista />} />
+            <Route path="*" element={<NotFound />}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
